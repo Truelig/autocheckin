c1fdbe41683c19a2555d3ee66c4ca450eb9d8d27
tuzg <12306@qq.com>
2023-09-04 17:55:35 +0800

    sss

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
+import json
+
+import requests
+
 print(666)
+
+pushplush_token = '7b9d76c0804a43fb95d269a0281c42e0'
+body = {
+    "token": pushplush_token,
+    "title": "掘金签到通知",
+    "content": '测试数据666',
+    "template": "txt"
+}
+requests.post('http://www.pushplus.plus/send', data=json.dumps(body))
